85dd08a7abb3569e307a6087dd0a958c50eb62fb
Hwang Seongchan <dbfpzk142@Gmail.com>
2021-10-29 18:59:07 +0900

    create member folder

--- init.bat
+++ init.bat
@@ -1,3 +1,13 @@
-set name=week
-FOR /L %%i IN (1, 1, 10) DO mkdir %name%%%i
-FOR /L %%i IN (1, 1, 10) DO fsutil file createnew ./%name%%%i/ghost.txt 0
+set folderName=week
+set member=영우 다롬 규태 예림 유진 성찬
+FOR /L %%i IN (1, 1, 10) DO (
+  mkdir %folderName%%%i
+  cd %folderName%%%i
+  FOR %%x IN (%member%) DO (
+    mkdir %%x
+    cd %%x
+    fsutil file createnew dummy.txt 0    
+    cd ..
+  )
+  cd ..
+)
